4cf3710633116a976682e1a623a8f4a6f1a8eef8
Ines Guerrero <inesgs@gmail.com>
2019-08-30 22:01:38 +0100

    Unit 2

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,46 @@
+// Variables and Data Types
+
+// var firstName = "John"; // string data type
+// var lastName = "Smith";
+// var age = 28; //number data type
+
+// In JS, numbers are always float numbers even if the decimals are not defined.
+
+// boolean
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job);
+// job = "Teacher";
+// console.log(job);
+
+// all variable names must start with a _, $, or a letter.
+// They can't start with reserved JS words
+
+//--------
+/* Variable Mutation and Type Coercion --------------------------
+-----------------------------------------------------------------*/
+
+//Type Coercion
+// var firstName = "John";
+// var age = 28;
+// console.log(firstName + " " + age);
+// // JS automatica coerced different types of data to combine them
+
+// var job, isMarried;
+// job = "teacher";
+// isMarried = false;
+
+// // Variable Mutation
+// age = "twenty eight";
+// job = "driver";
+
+// alert(firstName + " is a " + age + " year old " + job);
+
+// var lastName = prompt("What is his last name?");
+// console.log(firstName + " " + lastName);
+
+/* Basic Operators ----------------------------------------------
+-----------------------------------------------------------------
+----------------------------------------------------------------*/ 
